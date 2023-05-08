2b15ab136679008c41d15075fadd64809eb1e6be
yet1009 <84160197+yet1009@users.noreply.github.com>
2023-05-08 21:57:56 +0900

    qwerqwer

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+export const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+
+const Main = () => {
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+root.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
